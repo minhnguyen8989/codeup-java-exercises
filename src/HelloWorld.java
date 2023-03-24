cf804608b9804d3ab4ca380af842b0f26308cf03
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-24 12:16:54 -0500

    exercise experiment

---
+++ src/HelloWorld.java
@@ -0,0 +1,87 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        int myFavortiteNumber = 18;
+        System.out.println(myFavortiteNumber);
+
+        String myString = "Hello, I'm Minh";
+        System.out.println(myString);
+
+        /**
+         *Change your code to assign a character value to myString. What do you notice?
+         * In order to assign a character it's variable should be 'char'*/
+
+        /**
+         * Change your code to assign the value 3.14159 to myString. What happens?
+         * myString was assigned String variable and can not take in a number value unless
+         * the number is a string.*/
+
+        float myNumber;
+//        System.out.println(myNumber);
+        /**
+         * variable myNumber was not initialized because it have not assigned any value.*/
+
+        myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        /**
+         *3.14 was a floating number and long variable is declared to an integer number */
+
+        /**
+         * Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+         * One way to fixed line 23 is change float variable to double in line 18
+         * and another way is to put letter 'F' at the end of number value*/
+
+//        int y = 5;
+//        System.out.println(y++);
+//        System.out.println(y);
+
+        /**
+         * The code above was post-increment to the value
+         * mean: it will print value first then assign the value*/
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        /**
+         * The code above was Pre-increment to the value
+         * mean: it will assign the value first then print the value*/
+
+//        class classClass;
+        /**
+         * { expected
+         * Object?*/
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+        /**
+         * can not assign int variable to a string value even when value converted to int*/
+
+//        int four = (int) "four";
+//        System.out.println(four);
+
+        /**
+         * Same as above - same error */
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(y);
+
+
+    }
+
+}
