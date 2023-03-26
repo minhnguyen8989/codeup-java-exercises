5048dddc8bb156940378458f0e32334d136552d3
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-26 09:48:09 -0500

    exercise scanner 1, input error when input is not integer

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args){
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %s", pi);
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %s", pi);
+
+
+//        Scanner scanUserInput = new Scanner(System.in);
+//        System.out.print("Enter your classroom WIDTH: ");
+//        int userInputWidth = scanUserInput.nextInt();
+//
+//        System.out.print("Enter your classroom HEIGHT: ");
+//        int userInputHeight = scanUserInput.nextInt();
+//
+//        System.out.printf("you enter the width of %s, and height of %s", userInputWidth, userInputHeight);
+
+        /**
+         * System error when input is not integer*/
+
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter something: ");
+//        String userInput = scanner.next();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
     }
 }
