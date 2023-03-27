e3d11c3fdb64758ce8cfdfc4880b1722c861918b
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-27 10:03:16 -0500

    Exercise completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,39 +6,47 @@ public class ConsoleExercises {
 //        System.out.printf("The value of pi is approximately %s", pi);
 
 
-//        Scanner scanUserInput = new Scanner(System.in);
-//        System.out.print("Enter your classroom WIDTH: ");
-//        int userInputWidth = scanUserInput.nextInt();
-//
-//        System.out.print("Enter your classroom HEIGHT: ");
-//        int userInputHeight = scanUserInput.nextInt();
-//
-//        System.out.printf("you enter the width of %s, and height of %s", userInputWidth, userInputHeight);
+
 
         /**
          * System error when input is not integer*/
 
-        Scanner threeWordsInput = new Scanner(System.in);
-        System.out.print("Enter your first Word: ");
-        String userFirstWord = threeWordsInput.nextLine();
-
-        System.out.print("Enter your second Word: ");
-        String userSecondWord = threeWordsInput.nextLine();
-
-        System.out.print("Enter your third Word: ");
-        String userThirdWord = threeWordsInput.nextLine();
-
-        System.out.printf("The 3 words are: %s, %s, %s", userFirstWord, userSecondWord, userThirdWord);
-
-
-
-
-//        Scanner scanner = new Scanner(System.in);
+//        Scanner threeWordsInput = new Scanner(System.in);
+//        System.out.print("Enter your first Word: ");
+//        String userFirstWord = threeWordsInput.next();
 //
-//        System.out.print("Enter something: ");
-//        String userInput = scanner.next();
+//        System.out.print("Enter your second Word: ");
+//        String userSecondWord = threeWordsInput.next();
 //
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.print("Enter your third Word: ");
+//        String userThirdWord = threeWordsInput.next();
+//
+//        System.out.println(userFirstWord);
+//        System.out.println(userSecondWord);
+//        System.out.println(userThirdWord);
+//
+//        Scanner sentenceInput = new Scanner(System.in);
+//        System.out.print("Enter a sentence: ");
+//        String sentenceUserInput = sentenceInput.nextLine();
+//        System.out.print(sentenceUserInput);
+
+
+        Scanner scanUserInput = new Scanner(System.in);
+        System.out.print("Enter your classroom WIDTH: ");
+        String userInputWidth = scanUserInput.nextLine();
+
+        System.out.print("Enter your classroom HEIGHT: ");
+        String userInputHeight = scanUserInput.nextLine();
+
+        System.out.printf("you enter the width of %s, and height of %s ", userInputWidth, userInputHeight);
+        System.out.println("Area of a rectangle is: " + (Integer.parseInt(userInputHeight) * Integer.parseInt(userInputWidth)));
+        System.out.println("Perimeter of a rectangle is: " + ((Integer.parseInt(userInputHeight)*2) + (Integer.parseInt(userInputWidth)*2)));
+
+
+
+
+
+
 
     }
 }
