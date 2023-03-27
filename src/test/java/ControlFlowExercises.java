151766808e9f54f343a607ef9bff7bff6b234484
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-27 12:24:59 -0500

    For loop completed:

--- src/test/java/ControlFlowExercises.java
+++ src/test/java/ControlFlowExercises.java
@@ -1,12 +1,10 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        //WHile loop
-//        int i = 5;
-//        while(i <= 15){
-//            System.out.println(i);
-//            i++;
-//        }
+        //WHile loop --> for loop
+        for (int i = 5; i <= 15; i++){
+            System.out.println(i);
+        }
 
 
         //Do while loop
@@ -22,10 +20,8 @@ public class ControlFlowExercises {
 //            i -= 5;
 //        } while(i >= -10);
 
-        long i = 2;
-        do {
+        for (long i = 2; i < 1000000; i*=i){
             System.out.println(i);
-            i *= i;
-        }while (i < 1000000);
+        }
     }
 }
