ac7adb0da78e43e806cec9143cbc22582461c77c
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-27 12:12:47 -0500

    Do while loop count by 2's

--- src/test/java/ControlFlowExercises.java
+++ src/test/java/ControlFlowExercises.java
@@ -1,10 +1,19 @@
 public class ControlFlowExercises {
-    public static void main(String[] args){
-        int i = 5;
-        while(i <= 15){
-            System.out.println(i);
-            i++;
-        }
 
+    public static void main(String[] args) {
+        //WHile loop
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+        //Do while loop
+        int i = 0;
+        do {
+            System.out.println(i);
+            i += 2;
+        } while (i <= 100);
     }
 }
