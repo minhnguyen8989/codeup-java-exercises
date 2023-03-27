9791d710f86f01656462dc87d2fa658e00a7d42f
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-27 15:44:49 -0500

    String exercise

---
+++ src/test/java/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String firstStatement = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(firstStatement);
+    }
+}
+
