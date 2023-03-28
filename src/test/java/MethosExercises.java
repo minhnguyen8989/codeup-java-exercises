996c0caea7d17f11f37a5eb3ed50e53c92554f14
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-28 12:08:55 -0500

    moving to bonus

---
+++ src/test/java/MethosExercises.java
@@ -0,0 +1,24 @@
+public class MethosExercises {
+    public static void main(String[] args) {
+        System.out.println(addition(3, 5));
+        System.out.println(subtraction(6, 3));
+        System.out.println(multiplication(4, 8));
+        System.out.println(division(10, 2));
+    }
+
+    public static int addition(int x, int y){
+        return x + y;
+    }
+
+    public static int subtraction (int x, int y){
+        return x - y;
+    }
+
+    public static int multiplication (int x, int y){
+        return x * y;
+    }
+
+    public static int division (int x, int y){
+        return x / y;
+    }
+}
