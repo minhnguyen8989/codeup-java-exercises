9709e01731757126597cf89c8787832289852baa
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-28 09:25:19 -0500

    Bob exercise completed

---
+++ src/test/java/Bob.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner talk2Bob = new Scanner(System.in);
+        System.out.print("Say something to Bob: ");
+        String words2Bob = talk2Bob.nextLine();
+
+        if(words2Bob.endsWith("?")){
+            System.out.println("Sure");
+        } else if (words2Bob.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (words2Bob.equals("")) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever");
+        }
+
+    }
+}
+
