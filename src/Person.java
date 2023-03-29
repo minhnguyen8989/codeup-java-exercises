be8cc98f77d7f17d8bdcd42bc57aaefe4821408c
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-29 14:36:55 -0500

    Start Exercise

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name {
+
+    }
+
+    public String getName(){
+//TODO: return the person's name
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+    }
+
+}
--- src/test/java/MethosExercises.java
+++ src/test/java/MethosExercises.java
@@ -6,9 +6,12 @@ public class MethosExercises {
         System.out.println(subtraction(6, 3));
         System.out.println(multiplication(4, 8));
         System.out.println(division(10, 2)); //Can NOT divide by zero.
-        multiplicationWLoop(1);
+        multiplicationWLoop(6);
 
-        getInteger(1, 100);
+//        getInteger(1, 67);
+
+        getInterger(10);
+
     }
 
     public static int addition(int x, int y) {
@@ -36,16 +39,29 @@ public class MethosExercises {
         System.out.println(n);
     }
 
-    public static int getInteger(int min, int max) {
-        Scanner scanInput = new Scanner(System.in);
-        System.out.printf("Enter a number between %s and %s: ",min,max);
-        int userInput = scanInput.nextInt();
-        if (userInput >= min && userInput <= max) {
-            System.out.println("Thank you for your cooperation");
-            return userInput;
-        }
-        System.out.println("Please input valid number");
-        return getInteger(min, max);
+//    public static int getInteger(int min, int max) {
+//        Scanner scanInput = new Scanner(System.in);
+//        System.out.printf("Enter a number between %s and %s: ",min,max);
+//        int userInput = scanInput.nextInt();
+//        if (userInput >= min && userInput <= max) {
+//            System.out.println("Thank you for your cooperation");
+//            return userInput;
+//        }
+//        System.out.println("Please input valid number");
+//        return getInteger(min, max);
+//    }
+
+    public static long getInterger(long n){
+        Scanner scannerInput = new Scanner(System.in);
+        System.out.printf("Enter an integer from 1 to %s: ",n);
+        long userInput = scannerInput.nextLong();
+        for(int i = 1; i <= userInput; i++){
+            for(int j = 1; j < i; j++) {
+                System.out.print(j + " x ");
+                }
+            System.out.println(i);
+            }
+        return n;
     }
 }
 
