38e2efab69adcc942aec49b0aa0384389ddb7425
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-28 19:33:54 -0500

    Exercise 2 completed

--- src/test/java/MethosExercises.java
+++ src/test/java/MethosExercises.java
@@ -1,35 +1,51 @@
+import java.util.Scanner;
+
 public class MethosExercises {
     public static void main(String[] args) {
         System.out.println(addition(3, 5));
         System.out.println(subtraction(6, 3));
         System.out.println(multiplication(4, 8));
         System.out.println(division(10, 2)); //Can NOT divide by zero.
-        multiplicationWOLoop(1);
+        multiplicationWLoop(1);
 
+        getInteger(1, 100);
     }
 
-    public static int addition(int x, int y){
+    public static int addition(int x, int y) {
         return x + y;
     }
 
-    public static int subtraction (int x, int y){
+    public static int subtraction(int x, int y) {
         return x - y;
     }
 
-    public static int multiplication (int x, int y){
+    public static int multiplication(int x, int y) {
         return x * y;
     }
 
-    public static int division (int x, int y){
+    public static int division(int x, int y) {
         return x / y;
     }
 
-    public static void multiplicationWOLoop (int n){
-        if (n >= 200){
+    public static void multiplicationWLoop(int n) {
+        if (n >= 200) {
             System.out.println("It passed 200");
             return;
         }
-        multiplicationWOLoop(n + n);
+        multiplicationWLoop(n + n);
         System.out.println(n);
     }
+
+    public static int getInteger(int min, int max) {
+        Scanner scanInput = new Scanner(System.in);
+        System.out.printf("Enter a number between %s and %s: ",min,max);
+        int userInput = scanInput.nextInt();
+        if (userInput >= min && userInput <= max) {
+            System.out.println("Thank you for your cooperation");
+            return userInput;
+        }
+        System.out.println("Please input valid number");
+        return getInteger(min, max);
+    }
 }
+
