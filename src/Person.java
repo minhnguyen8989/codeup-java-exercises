521f4574141f184ce03c46843036d704612a3311
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-30 13:08:30 -0500

    exercise #1 complete

--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,30 @@
 public class Person {
-    private String name {
-
-    }
+    private String name;
 
     public String getName(){
 //TODO: return the person's name
+        return this.name = name;
     }
 
     public void setName(String name){
 //TODO: change the name field to the passed value
+
     }
     public void sayHello(){
 //TODO: print a message to the console using the person's name
+        System.out.printf("%s said Hello! %n", name);
+    }
+
+    Person(String userName){
+        this.name = userName;
+    }
+
+
+    public static void main(String[] args) {
+        Person firstPerson = new Person("Minh");
+        Person secondPerson = new Person("David");
+        firstPerson.sayHello();
+        secondPerson.sayHello();
     }
 
 }
