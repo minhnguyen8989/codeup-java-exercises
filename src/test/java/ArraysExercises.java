f68c50ca24f777419ff0deaa75c0c1a40581b9b1
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-03 10:48:39 -0500

    Exercise 2 complete

--- src/test/java/ArraysExercises.java
+++ src/test/java/ArraysExercises.java
@@ -1,33 +1,32 @@
-import java.util.Arrays;
-
-public class ArraysExercises {
-
-    public static void main(String[] args) {
-
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
-
-
-        String[] Person = new String[3];
-        Person[0] = "Minh";
-        Person[1] = "Van";
-        Person[2] = "Elena";
-        for (String name : Person){
-            System.out.println(name);
-        }
-
-        addPerson("Cuong");
-
-    }
-
-    static String[] addPerson(String[] name2Add){
-
-        String[] moreName = new String[name2Add.length+1];
-        for (int i = 0; i < name2Add.length; i++){
-            moreName[i] = name2Add[i];
-        }
-
-    }
-
-}
+//import java.util.Arrays;
+//
+//public class ArraysExercises {
+//
+//    public static void main(String[] args) {
+//
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
+//
+//
+//        String[] Person = new String[3];
+//        Person[0] = "Minh";
+//        Person[1] = "Van";
+//        Person[2] = "Elena";
+//        for (String name : Person){
+//            System.out.println(name);
+//        }
+//
+//
+//    }
+//
+//    static String[] addPerson(String[] name2Add){
+//
+//        String[] moreName = new String[name2Add.length+1];
+//        for (int i = 0; i < name2Add.length; i++){
+//            moreName[i] = name2Add[i];
+//        }
+//        reutnr
+//    }
+//
+//}
 
---
+++ src/test/java/ServerNameGenerator.java
@@ -0,0 +1,19 @@
+public class ServerNameGenerator {
+
+    public static String randomNouns() {
+        String[] nouns = {"dog", "cat", "car", "book", "phone", "computer", "house", "tree", "coffee", "pen"};
+        int randomIndex1 = (int) (Math.random() * nouns.length);
+        return nouns[randomIndex1];
+    }
+
+    public static String randomAdjective() {
+        String[] adjectives = {"happy", "sad", "funny", "serious", "crazy", "curious", "beautiful", "ugly", "smart", "dumb"};
+        int randomIndex2 = (int) (Math.random() * adjectives.length);
+        return adjectives[randomIndex2];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.printf("The %s is very %s %n", randomNouns(), randomAdjective());
+    }
+}
