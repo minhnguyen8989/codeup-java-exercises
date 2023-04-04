095adf61b188c98536920ebb5137b617e7de4eba
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-04 10:06:44 -0500

    Added movie array list

--- src/test/java/Array/ArraysExercises.java
+++ src/test/java/Array/ArraysExercises.java
@@ -24,7 +24,6 @@ public class ArraysExercises {
         String newName = scanName.nextLine();
 
         return newName;
-
     }
 
 }
---
+++ src/test/java/movies/Movie.java
@@ -0,0 +1,13 @@
+package movies;
+public class Movie {
+
+    private String[] name;
+    private String[] category;
+
+    public Movie(String[] name, String[] category) {
+        this.name = name;
+        this.category = category;
+    }
+
+
+}
